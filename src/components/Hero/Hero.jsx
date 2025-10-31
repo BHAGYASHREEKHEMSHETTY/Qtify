7ae3dbde1a9f24dbdcf1564ca8b74e5d206c68a1
BHAGYASHREEKHEMSHETTY <bhagyanelge@gmail.com>
2025-10-31 21:10:16 +0530

    Initial commit2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,15 @@
+import React from "react";
 import styles from "./hero.module.css";
 import heroImage from "../../assets/hero.svg";
 
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-      <div>
+    <div data-testid="hero-section" className={styles.hero}>
+      <div className={styles.heroText}>
         <h1>100 Thousand Songs, ad-free</h1>
         <h1>Over thousands of podcast episodes</h1>
       </div>
-      <img src={heroImage} alt="Hero_Image" />
+      <img src={heroImage} alt="hero" className={styles.heroImage} />
     </div>
   );
 };
