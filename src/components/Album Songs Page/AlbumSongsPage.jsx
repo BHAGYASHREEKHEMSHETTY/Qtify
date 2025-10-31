89c02db551ed30bcd9d9e3b6f74fe2a9778e5e21
BHAGYASHREEKHEMSHETTY <bhagyanelge@gmail.com>
2025-10-31 20:29:49 +0530

    Initial commit

--- src/components/Album Songs Page/AlbumSongsPage.jsx
+++ src/components/Album Songs Page/AlbumSongsPage.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { fetchTopAlbum, fetchNewAlbum } from "../../api/api";
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,20 @@
 import navbarStyles from "../Navbar/navbar.module.css";
 
 const Button = ({ text, eventHandler }) => {
+  const handleClick = (e) => {
+    if (eventHandler?.event === "onClick" && typeof eventHandler.handler === "function") {
+      eventHandler.handler(e);
+    }
+  };
+
   return (
-    <div>
-      <button
-        className={navbarStyles.feedback}
-        onClick={(e) =>
-          eventHandler.event === "onClick" && eventHandler.handler(e)
-        }
-      >
-        {text}
-      </button>
-    </div>
+    <button
+      data-testid="feedback-button" // ✅ helps automated tests locate button
+      className={navbarStyles.feedback}
+      onClick={handleClick}
+    >
+      {text}
+    </button>
   );
 };
 
